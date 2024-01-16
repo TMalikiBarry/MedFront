ca40dc421283f06a5011ae9904ea2e3cd664143a
Babacar NIANG <babacar.niang@intouchgroup.net>
2024-01-16 13:01:03 +0000

    Add NouveauPatientComponent and its related files

---
+++ src/app/Common/personnes/components/nouveau-patient/nouveau-patient.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nouveau-patient',
+  templateUrl: './nouveau-patient.component.html',
+  styleUrls: ['./nouveau-patient.component.sass']
+})
+export class NouveauPatientComponent {
+
+}
