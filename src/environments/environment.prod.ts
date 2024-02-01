25827c270e0c7605af4f627f7d77b884bec1b5e2
babacar.adje <babacar.adje@intouchgroup.net>
2024-02-01 10:06:02 +0000

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  // apiURL : 'http://localhost:8090/touchmed-api',
+   //apiURL : 'http://localhost:8090/touchmed-api',
   // apiURL : 'http://52.210.42.160:8084/touchmed-api',
   apiURL : 'https://dev-touchmed.gutouch.net/touchmed-api',
 };
