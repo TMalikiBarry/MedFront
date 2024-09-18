4594ce776e481d1638ae685b911f09fda4532bab
zeynalaguerriere <seynabou.ndiaye@intouchgroup.net>
2024-09-18 10:50:47 +0000

    &affichage de la liste des profils

--- src/app/Common/securite/components/profil/profil.component.ts
+++ src/app/Common/securite/components/profil/profil.component.ts
@@ -1,10 +1,150 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Page} from "../../../../models/pagination.interface";
+import {NzModalService} from "ng-zorro-antd/modal";
+import {UtilsService} from "../../../../services/utils/utils.service";
+import {NotifService} from "../../../../services/notification/notif.service";
+import {NzTableQueryParams} from "ng-zorro-antd/table";
+import {NouvelleFonctionnaliteComponent} from "../../dialogs/nouvelle-fonctionnalite/nouvelle-fonctionnalite.component";
+import {ProfilService} from "../../../../services/Profil/profil.service";
+import {ProfilInterface} from "../../../../models/profil.interface";
+import {ProfilFormDialogComponent} from "../../dialogs/profil-form-dialog/profil-form-dialog.component";
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.sass']
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit{
 
+
+  profils!: ProfilInterface[];
+  pageIndex: number = 0;
+  pageSize: number = 10;
+
+  serviceId!: number;
+
+  paginatedData!: Page<ProfilInterface>;
+
+  constructor(private profilService: ProfilService,
+              private modalService: NzModalService,
+              public utils: UtilsService,
+              private notify: NotifService
+  ) {}
+
+  ngOnInit() {
+    this.loadProfil();
+    this.getProfilByPage();
+
+  }
+
+  getProfilByPage(page: number = 0, size: number = 10, firstName?: string, lastName?: string, telephone?: string,
+                          startDate?: Date, endDate?: Date, status?: string, genre?: string) {
+
+    this.profilService.getPaginatedFilteredData(page, size, firstName, lastName,
+      telephone, startDate, endDate, status, genre)
+      .subscribe({
+        next: response => {
+          console.log(response)
+          this.paginatedData = response;
+
+        }
+      })
+  }
+
+  onQueryParamsChange(params: NzTableQueryParams): void {
+    this.pageIndex = params.pageIndex - 1;
+    this.pageSize = params.pageSize;
+
+    this.filterData();
+  }
+
+  filterData() {
+    let startDate = undefined;
+    let endDate = undefined;
+
+    let prenom = null;
+    let nom = null;
+    let telephone = null;
+
+
+    console.log('RECUPERER LES DONNEES');
+    this.getProfilByPage(this.pageIndex, this.pageSize, prenom!, nom!,
+      telephone!, startDate, endDate);
+    console.log('BIEN RECU LES DONNEES');
+
+  }
+
+  loadProfil() {
+    this.profilService.getAll().subscribe({
+        next: profils => {
+          this.profils = profils;
+          console.log('Recuperation des profils', profils);
+        },
+        error: (error) => {
+          console.error('Erreur lors de la récupération des profils', error);
+          // Gérez l'erreur selon vos besoins
+        }
+      }
+    );
+  }
+
+  addNewProfil() {
+    this.modalService.create({
+      nzContent: ProfilFormDialogComponent,
+      nzClosable: false,
+      nzWidth: 750,
+      nzCentered: true,
+    }).afterClose.subscribe(
+      (result) => {
+        if (result)
+          this.filterData();
+      }
+    );
+  }
+
+  updateProfil(code: string) {
+    this.modalService.create({
+      nzContent: ProfilFormDialogComponent,
+      nzClosable: false,
+      nzWidth: 750,
+      nzCentered: true,
+      nzData: code
+    }).afterClose.subscribe(
+      (result) => {
+        if (result)
+          this.filterData();
+      }
+    );
+  }
+
+
+
+  deleteProfil(code: string) {
+    this.modalService.confirm({
+      nzTitle: 'Êtes-vous sûr de vouloir supprimer ce profil ?',
+      nzContent: 'Cette action est irréversible.',
+      nzOkText: 'Oui',
+      nzOkDanger: true,
+      nzCancelText: 'Non',
+      nzOnOk: () => {
+        this.profilService.delete(code).subscribe({
+          next: response => {
+            let code = response;
+            console.log("Reponse suppression")
+            console.log(response)
+            this.notify.snackMessage(`Suppression effectuée avec succés `, 3000, "success");
+          }
+        })
+        console.log('Suppression confirmée');
+      },
+      nzOnCancel: () => {
+        console.log('Suppression annulée');
+      }
+    }).afterClose.subscribe(
+      (result) => {
+        if (result)
+          this.filterData();
+      }
+    );
+  }
 }
--- src/app/services/Profil/profil.service.ts
+++ src/app/services/Profil/profil.service.ts
@@ -3,40 +3,77 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../../../environments/environment.prod";
 import {ApiResponseInterface} from "../../models/api-response.interface";
 import {ProfilInterface} from "../../models/profil.interface";
+
+import {UtilsService} from "../utils/utils.service";
+import {Observable} from "rxjs";
 import {Page} from "../../models/pagination.interface";
-import {PersonnelInterface} from "../../models/personnel.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfilService {
 
-  private readonly url = environment.apiURL + '/profil';
+  constructor(private http: HttpClient, private utils: UtilsService) { }
+  private readonly url = `${environment.apiURL}/profil`;
+  getPaginatedFilteredData(page: number = 0, size: number = 10, firstName?: string, lastName?: string,
+                           telephone?: string, startDate?: Date, endDate?: Date,
+                           status?: string, genre?: string, ageMin?: number, ageMax?: number) {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
 
-  private readonly urlAction = `${environment.apiURL}/action`;
+    if (firstName) {
+      params = params.set('firstName', firstName);
+    }
+    if (lastName) {
+      params = params.set('lastName', lastName);
+    }
 
-  constructor(private http: HttpClient) {
+    if (this.utils.numberIsDefined(ageMin!)) {
+      params = params.set('ageMin', ageMin!);
+    }
+
+    if (this.utils.numberIsDefined(ageMax!)) {
+      params = params.set('ageMax', ageMax!);
+    }
+
+    if (telephone) {
+      params = params.set('telephone', telephone);
+    }
+    if (status) {
+      params = params.set('status', status);
+    }
+    if (genre) {
+      params = params.set('genre', genre);
+    }
+    if (startDate) {
+      params = params.set('startDate', startDate.toISOString());
+    }
+    if (endDate) {
+      params = params.set('endDate', endDate.toISOString());
+    }
+
+    return this.http.get<Page<ProfilInterface>>(this.url, {params});
   }
 
-  getPaginatedData(page: number = 0, size: number = 10) {
-    // Création des paramètres de la requête
-    let params = new HttpParams();
-    params = params.append('page', page.toString());
-    params = params.append('size', size.toString());
-
-    // Envoi de la requête GET avec les paramètres de pagination
-    return this.http.get<Page<PersonnelInterface>>(this.url, {params});
+  getAll(){
+    return  this.http.get<any>(this.url+"/all")
   }
 
-  getAll() {
-    return this.http.get<ApiResponseInterface>(this.url + '/allPresent');
+  update( profil: ProfilInterface, profilID: number): Observable<ApiResponseInterface>
+  {
+    return this.http.put<ApiResponseInterface>(this.url+"/update"+`/${profilID}`, profil);
   }
 
-  save(p: ProfilInterface) {
-    return this.http.post<ApiResponseInterface>(this.url + '/save', p);
+  getProfilByCode(code: string): Observable<ApiResponseInterface> {
+    return this.http.get<ApiResponseInterface>(`${this.url}/${code}`);
   }
 
-  update(p: ProfilInterface, pID: number) {
-    return this.http.put(this.url + '/update/' + pID, p);
+  save(module: ProfilInterface): Observable<ApiResponseInterface> {
+
+    return this.http.post<ApiResponseInterface>(this.url+"/save", module);
+  }
+  delete(code: string): Observable<any> {
+    return this.http.delete(`${this.url}/${code}`, {responseType: 'text'});
   }
 }
