2fbd54a0c2e2d79c92d52e657a96969455415dc8
Thierno Maliki BARRY <thiernohacker@gmail.com>
2024-10-10 10:42:46 +0000

    Config for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
 
-  apiURL : 'http://localhost:8090/touchmed-api',
-  //apiURL : 'https://dev-touchmed.gutouch.net/touchmed-api',
+  // apiURL : 'http://localhost:8090/touchmed-api',
+  apiURL: 'https://dev-touchmed.gutouch.net/touchmed-api',
   //apiURL : 'http://172.16.16.103:8090/touchmed-api/',
   /*apiURL : 'https://prod-touchmed.gutouch.net/touchmed-api'*/
 };
